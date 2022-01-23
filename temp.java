87453a1edfc84e358fd40464a5a5d26391c5d95a
Rudeecho <me19s016@smail.iitm.ac.in>
2022-01-23 11:47:39 +0530

    removed the java file

--- temp.java
+++
@@ -1,8 +0,0 @@
-public class temp{
-    public static void main(String[] args){
-        int a = 12;
-        int b = 5;
-        int c = a*a + b*b;
-        System.out.println("The value of c is " + c);
-    }
-}
